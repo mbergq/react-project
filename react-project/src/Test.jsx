f6967c91b0ca396084859945cafaba76972e753d
Martin <106612709+menkodot@users.noreply.github.com>
2024-05-30 16:56:03 +0200

    setup page nav form with formik

--- react-project/src/Test.jsx
+++ react-project/src/Test.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import styled from "styled-components";
 import axios from "axios";
 const url = "https://api.harvardartmuseums.org/image?";
@@ -9,26 +9,31 @@ const Image = styled.img`
   height: auto;
   object-fit: cover;
 `;
-const ImageWrapper = styled.div`
+const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(0, 6vw);
   grid-gap: 15px;
 `;
-const Div = styled.div`
+const GridWrapper = styled.div`
   max-width: 60%;
+`;
+const Div = styled.div`
+  border: solid 1px black;
 `;
 function Test() {
   const [data, setData] = useState(null);
+  // const [pageNumber, setPageNumber] = useState(1);
 
-  const fetchData = async () => {
+  const fetchData = async (n) => {
+    console.log(n);
     try {
       //fetch api-key from local json file
       const response = await fetch("../apikey.json");
       const json = await response.json();
       //fetch the array of data needed and put it into state
       return axios
-        .get(`${url}apikey=${json.apikey}&size=100&page=32`)
+        .get(`${url}apikey=${json.apikey}&size=100&page=${n}`)
         .then((response) => {
           console.log(response.data.records);
           setData(response.data.records);
@@ -45,15 +50,50 @@ function Test() {
 
   return (
     <>
-      <h2>Test</h2>
+      <h2>Go to a page</h2>
       <Div>
-        <ImageWrapper>
+        <Formik
+          initialValues={{ name: "" }}
+          validate={(values) => {
+            const errors = {};
+            let numbers = /^[0-9]+$/;
+            if (!values.name.match(numbers) && values.name !== "") {
+              errors.name = "Must be a number";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log(values.name);
+            fetchData(values.name);
+            setSubmitting(false);
+            // setTimeout(() => {
+            //   alert(JSON.stringify(values, null, 2));
+
+            //   setSubmitting(false);
+            // }, 400);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Field type="name" name="name" />
+
+              <ErrorMessage name="name" component="p" />
+
+              <button type="submit" disabled={isSubmitting}>
+                Go
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </Div>
+      <GridWrapper>
+        <Grid>
           {data !== null &&
             data.map((object) => (
               <Image key={object.id} src={object.baseimageurl}></Image>
             ))}
-        </ImageWrapper>
-      </Div>
+        </Grid>
+      </GridWrapper>
     </>
   );
 }
